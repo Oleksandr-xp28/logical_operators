494fc6af474783e1be4bce8a76f589db9ae896e5
Oleksandr-xp28 <fedorut20@gmail.com>
2023-05-26 18:55:27 +0300

    Add project files.

---
+++ logical_operators.cpp
@@ -0,0 +1,16 @@
+// logical_operators.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <windows.h>
+#include <cmath>
+#include <iomanip>
+using namespace std;
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
