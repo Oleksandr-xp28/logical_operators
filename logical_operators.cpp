5fa441f9cc8a9ef84a922393836670f9794309ce
Oleksandr-xp28 <fedorut20@gmail.com>
2023-05-26 20:12:08 +0300

    update

--- logical_operators.cpp
+++ logical_operators.cpp
@@ -7,10 +7,29 @@
 #include <iomanip>
 using namespace std;
 
-int main()
-{
-    std::cout << "Hello World!\n";
+int main() {
+    double cargoWeight;
+    std::cout << "Enter the weight of the cargo in kg: ";
+    std::cin >> cargoWeight;
+
+    double fuelConsumption;
+
+    if (cargoWeight <= 500) {
+        fuelConsumption = cargoWeight * 1; // 1 liter/km
+    }
+    else if (cargoWeight <= 1000) {
+        fuelConsumption = cargoWeight * 4; // 4 liters/km
+    }
+    else {
+        std::cout << "Cargo weight exceeds the maximum capacity." << std::endl;
+        return 0;
+    }
+
+    std::cout << "The fuel consumption for the given cargo weight is: " << fuelConsumption << " liters/km" << std::endl;
+
+    return 0;
 }
+
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
 // Debug program: F5 or Debug > Start Debugging menu
