d3b840f7f233bb2914b8390f203d292479c6ffd8
Oleksandr-xp28 <fedorut20@gmail.com>
2023-05-26 20:28:54 +0300

    Not done

--- logical_operators.cpp
+++ logical_operators.cpp
@@ -7,10 +7,24 @@
 #include <iomanip>
 using namespace std;
 
-int main()
-{
-    std::cout << "Hello World!\n";
+
+int main() {
+    int day1, month1, year1;
+    int day2, month2, year2;
+
+    std::cout << "Enter the first date (day month year): ";
+    cin >> day1 >> month1 >> year1;
+
+    cout << "Enter the second date (day month year): ";
+    cin >> day2 >> month2 >> year2;
+
+    int daysBetween = (day1, month1, year1, day2, month2, year2);
+
+    cout << "Number of days between the two dates: " << daysBetween << std::endl;
+
+    return 0;
 }
+
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
 // Debug program: F5 or Debug > Start Debugging menu
